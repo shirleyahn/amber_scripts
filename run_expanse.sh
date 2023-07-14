28f0d5aa63e1c1e8c9cbd756f2a40cf48f5ae386
shirleyahn <sahn@ucdavis.edu>
2023-07-14 16:37:30 -0700

    added run script

---
+++ run_expanse.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name="run"
+#SBATCH --output="run.%j.%N.out"
+#SBATCH --partition=gpu-shared
+#SBATCH --nodes=1
+#SBATCH --gpus=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=50G
+#SBATCH --account=csd743
+#SBATCH --no-requeue
+#SBATCH --mail-user=sahn@ucdavis.edu
+#SBATCH --mail-type=ALL
+#SBATCH -t 48:00:00
+
+module purge
+module load shared
+module load gpu/0.15.4
+module load slurm
+module load openmpi/4.0.4
+module load cuda10.2/toolkit/10.2.89
+module load amber/20
+
+export PATH=$PATH:$HOME/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH
+source $AMBERHOME/amber.sh
+pmemd.cuda -O -i prod.in -o prod.out -p protein.prmtop -c equil.rst -r prod.rst -x prod.nc -inf prod.mdinfo
